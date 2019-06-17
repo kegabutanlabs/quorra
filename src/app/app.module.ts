e9c857b366047d1fa8b1aa0dd63b3530d5ee2b8b
Dewa Andhika Putra <dewaandhika18@gmail.com>
2019-06-17 22:43:32 +0700

    Generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './header/nav/nav.component';
+import { MainComponent } from './main/main.component';
+import { PostsComponent } from './main/posts/posts.component';
+import { PostListComponent } from './main/post-list/post-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    MainComponent,
+    PostsComponent,
+    PostListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
