9930851356d15186dfb83affdd72f12de5e8891b
Dewa Andhika Putra <dewaandhika18@gmail.com>
2019-06-27 07:14:06 +0700

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AdsenseModule } from 'ng2-adsense';
 
 import { HeaderModule } from './header/header.module';
 import { MainModule } from './main/main.module';
@@ -21,7 +22,11 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     MainModule,
     BrowserModule,
     AppRoutingModule,
-    MainRoutingModule
+    MainRoutingModule,
+    AdsenseModule.forRoot({
+      adClient: 'ca-pub-9305328163038270',
+      adSlot: 4536593384,
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
