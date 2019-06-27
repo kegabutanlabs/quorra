34baaf5fe4b5b4c65ff3e4eca1a2cd47bf448130
Dewa Andhika Putra <dewaandhika18@gmail.com>
2019-06-27 07:15:02 +0700

    update navigation

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { MainRoutingModule } from '../main/main-routing.module';
 
 import { NavComponent } from './nav/nav.component';
 import { HeaderComponent } from './header.component';
@@ -12,7 +15,10 @@ import { HeaderComponent } from './header.component';
   ],
   imports: [
     CommonModule,
-    BsDropdownModule.forRoot()
+    BrowserAnimationsModule,
+    BsDropdownModule.forRoot(),
+    CollapseModule.forRoot(),
+    MainRoutingModule
   ],
   exports: [
     HeaderComponent
