3adb99eaa2f243afa2a431e6b7be7e32ec519de4
Dewa Andhika Putra <dewaandhika18@gmail.com>
2019-06-28 06:52:28 +0700

    add: config file

---
+++ src/app/config.ts
@@ -0,0 +1,8 @@
+export const API_ENDPOINTS = {
+  blog: 'blogs',
+  post: 'posts',
+  page: 'pages'
+}
+export const API_KEY = 'YOUR-API-KEY';
+export const BLOG_ID = 4823400831779539490;
+export const API_URL = 'https://www.googleapis.com/blogger/v3/'
