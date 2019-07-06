791b04f5147ac3a089b2e22204a34e1f7edb205a
Dewa Andhika Putra <dewaandhika18@gmail.com>
2019-07-06 15:48:43 +0700

    update: app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderModule } from './header/header.module';
-import { MainModule } from './main/main.module';
-import { MainRoutingModule } from './main/main-routing.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
-        HeaderModule,
-        MainModule,
+        RouterTestingModule
       ],
       declarations: [
-        AppComponent,
-        FooterComponent
+        AppComponent
       ],
     }).compileComponents();
   }));
