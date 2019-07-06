39ce53838ead024fdbfc1d7d0ad77f85bc9ce0c0
Dewa Andhika Putra <dewaandhika18@gmail.com>
2019-07-06 15:57:18 +0700

    update: import ng-disqus module

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,28 +2,30 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { AdsenseModule } from 'ng2-adsense';
+import { DisqusModule } from "ngx-disqus";
+import { environment } from '../../environments/environment';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { PostComponent } from './post/post.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { AsideComponent } from './aside/aside.component';
+import { RefreshComponent } from './refresh/refresh.component';
 
 @NgModule({
   declarations: [
     MainComponent,
     PostComponent,
     PostListComponent,
-    AsideComponent
+    AsideComponent,
+    RefreshComponent
   ],
   imports: [
     CommonModule,
     HttpClientModule,
     MainRoutingModule,
-    AdsenseModule.forRoot({
-      adClient: 'YOUR-ADSENSE-CLIENT',
-      adSlot: 177013,
-    })
+    AdsenseModule.forRoot(environment.app.adsense),
+    DisqusModule.forRoot('kegabutandotcom')
   ],
   exports: [
     MainComponent
