699acf9355659dc7f997f4e96a3b23e123c3c84e
Dewa Andhika Putra <dewaandhika18@gmail.com>
2019-07-06 15:59:30 +0700

    update: main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor(private title: Title) { }
+
+  pageTitle = 'Kegabutan.com';
 
   ngOnInit() {
+    this.title.setTitle(this.pageTitle);
   }
 
 }
