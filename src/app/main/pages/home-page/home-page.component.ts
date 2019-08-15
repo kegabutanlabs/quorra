d6281073b787030f175fb8612486d6b9b630dfbe
Dewa Andhika Putra <dewaandhika18@gmail.com>
2019-08-16 00:25:56 +0700

    update to be able to passing data to post-list

--- src/app/main/pages/home-page/home-page.component.ts
+++ src/app/main/pages/home-page/home-page.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { PostListResponse } from '../../services/response.interface';
+import { PostService } from '../../services';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+    public postService: PostService
+  ) { }
+
+  private subscription: Subscription;
+  postData: any;
+  post$: Observable<PostListResponse> = this.postService.retrievePosts(5, true);
 
   ngOnInit() {
+    this.subscription = this.post$.subscribe((data: any) => {
+      this.postData = data;
+    });
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
