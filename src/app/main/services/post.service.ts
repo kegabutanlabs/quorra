9d1d74ea153d6c47fae6380e391285c0eb6840e4
Dewa Andhika Putra <dewaandhika18@gmail.com>
2019-08-17 03:04:27 +0700

    add search post method and change retrieve post param

--- src/app/main/services/post.service.ts
+++ src/app/main/services/post.service.ts
@@ -5,7 +5,7 @@ import { retry } from 'rxjs/operators';
 
 import { environment } from '../../../environments/environment';
 import { PostUrlService } from './post-url.service';
-import { PostListResponse, PostItemResponse } from './response.interface';
+import { PostListResponse, PostItemResponse, PostSearchResponse } from './response.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -30,8 +30,14 @@ export class PostService {
     this.apiEndpoint = `${apiUrl}blogs/${blogId}/${apiEndpoint}`;
   }
 
-  retrievePosts(maxResults: number, fetchBodies?: boolean): Observable<PostListResponse> {
+  retrievePosts(
+    maxResults: number,
+    fetchBodies?: boolean,
+    labels?: Array<string>,
+    orderBy?: string
+  ): Observable<PostListResponse> {
     const bodies = (typeof fetchBodies === 'undefined') ? true : fetchBodies;
+    const postLabels = (typeof labels === 'undefined') ? [] : labels;
     const paramOptions = {
       params: new HttpParams()
               .set('maxResults', maxResults.toString())
@@ -39,6 +45,8 @@ export class PostService {
               .set('fetchBodies', bodies.toString())
               .set('fetchImages', 'true')
               .set('key', this.apiKey)
+              .set('labels', postLabels.join(','))
+              .set('orderBy', orderBy ? orderBy : 'published')
     };
 
     return this.http.get<PostListResponse>(this.apiEndpoint, paramOptions)
@@ -60,6 +68,24 @@ export class PostService {
     };
 
     return this.http.get<PostItemResponse>(apiEndpoint, paramOptions)
+      .pipe(
+        retry(3)
+      );
+  }
+
+  searchPost(query: string, fetchBodies?: boolean, orderBy?: string): Observable<PostSearchResponse> {
+    const apiEndpoint = `${this.apiEndpoint}/search`;
+    const bodies = (typeof fetchBodies === 'undefined') ? true : fetchBodies;
+    const paramOptions = {
+      params: new HttpParams()
+              .set('q', query)
+              .set('prettyPrint', 'false')
+              .set('fetchBodies', bodies.toString())
+              .set('orderBy', orderBy ? orderBy : 'published')
+              .set('key', this.apiKey)
+    };
+
+    return this.http.get<PostSearchResponse>(apiEndpoint, paramOptions)
       .pipe(
         retry(3)
       );
