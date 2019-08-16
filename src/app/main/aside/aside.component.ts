bde05e84095a5a462ce3c01911075c8dafa23081
Dewa Andhika Putra <dewaandhika18@gmail.com>
2019-08-17 03:09:05 +0700

    update aside components

--- src/app/main/aside/aside.component.ts
+++ src/app/main/aside/aside.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { PostService } from '../services';
+import { Subscription, Observable } from 'rxjs';
+import { PostSearchResponse } from '../services/response.interface';
 
 @Component({
   selector: 'app-aside',
   templateUrl: './aside.component.html',
   styleUrls: ['./aside.component.scss']
 })
-export class AsideComponent implements OnInit {
+export class AsideComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+    public postService: PostService
+  ) { }
+
+  private subscription: Subscription;
+  postData: any;
+  post$: Observable<PostSearchResponse> = this.postService.searchPost('anime', false);
 
   ngOnInit() {
+    this.subscription = this.post$.subscribe((data: any) => {
+      this.postData = data;
+    });
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
