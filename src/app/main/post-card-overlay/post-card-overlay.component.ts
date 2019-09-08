843268cbfcfc88ef1bae97cfce35292a78ba94a9
Dewa Andhika Putra <dewaandhika18@gmail.com>
2019-09-08 15:09:58 +0700

    add post card overlay component

---
+++ src/app/main/post-card-overlay/post-card-overlay.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-card-overlay',
+  templateUrl: './post-card-overlay.component.html',
+  styleUrls: ['./post-card-overlay.component.scss']
+})
+export class PostCardOverlayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/main/shared.module.ts
+++ src/app/main/shared.module.ts
@@ -10,13 +10,15 @@ import { MainRoutingModule } from './main-routing.module';
 import { AdsenseModule } from 'ng2-adsense';
 import { environment } from 'src/environments/environment';
 import { DisqusModule } from 'ngx-disqus';
+import { PostCardOverlayComponent } from './post-card-overlay/post-card-overlay.component';
 
 @NgModule({
   declarations: [
     PostComponent,
     PostListComponent,
     RefreshComponent,
-    StripHtmlPipe
+    StripHtmlPipe,
+    PostCardOverlayComponent
   ],
   imports: [
     CommonModule,
@@ -29,6 +31,7 @@ import { DisqusModule } from 'ngx-disqus';
     PostComponent,
     PostListComponent,
     RefreshComponent,
+    PostCardOverlayComponent,
     StripHtmlPipe,
     HttpClientModule,
     AdsenseModule,
