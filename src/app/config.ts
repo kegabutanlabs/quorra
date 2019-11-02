271f9c2a0903d8e6926403445c7f89e9906370ee
Dewa Andhika Putra <dewaandhika18@gmail.com>
2019-11-02 17:54:35 +0700

    initial commit

---
+++ src/app/config.ts
@@ -0,0 +1,17 @@
+export const appConfigs = {
+  adsense: {
+    adClient: 'undefined', // change this line
+    adSlot: 'undefined', // change this line
+    pageLevelAds: undefined,
+    display: 'block'
+  },
+  API_ENDPOINTS: {
+    blog: '/blogs',
+    post: '/posts',
+    post_search: '/posts/search',
+    page: '/pages'
+  },
+  API_KEY: 'undefined', // change this line
+  BLOG_ID: 'undefined', // change this line
+  API_URL: 'https://www.googleapis.com/blogger/v3'
+};
